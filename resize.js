54c2442e688beebad9e337af76414c9ea7275429
LIAWCV <liawcv@gmail.com>
2024-03-30 00:52:13 +0800

    Add files via upload

---
+++ resize.js
@@ -0,0 +1,98 @@
+// PURPOSE: Center-crop image to the width and height specified (upscale)
+function crop(f, w, h) {
+    return new Promise((resolve, reject) => { 
+        const img = document.createElement('img');
+        
+        img.onload = e => {
+            URL.revokeObjectURL(img.src);
+            
+            // Resize algorithm ---------------------------
+            const ratio = w / h;
+            const nw = img.naturalWidth;
+            const nh = img.naturalHeight;
+            const nratio = nw / nh;
+
+            let sx, sy, sw, sh;
+
+            if (ratio >= nratio) {
+                // Retain width, calculate height
+                sw = nw;
+                sh = nw / ratio;
+                sx = 0;
+                sy = (nh - sh) / 2;
+            }
+            else {
+                // Retain height, calculate width
+                sw = nh * ratio;
+                sh = nh;
+                sx = (nw - sw) / 2;
+                sy = 0;
+            }
+            // --------------------------------------------
+
+            const can = document.createElement('canvas');
+            can.width  = w;
+            can.height = h;
+            can.getContext('2d').drawImage(img, sx, sy, sw, sh, 0, 0, w, h);
+
+            // Resolve to dataURL
+            resolve(can.toDataURL());
+        };
+
+        img.onerror = e => {
+            URL.revokeObjectURL(img.src);
+            reject('ERROR: File is not an image');
+        };
+
+        img.src = URL.createObjectURL(f);
+    });
+}
+
+// PURPOSE: Best-fit image within the width and height specified (no upscale)
+function fit(f, w, h) {
+    return new Promise((resolve, reject) => { 
+        const img = document.createElement('img');
+        
+        img.onload = e => {
+            URL.revokeObjectURL(img.src);
+            
+            // Resize algorithm ---------------------------
+            const ratio = w / h;
+            const nw = img.naturalWidth;
+            const nh = img.naturalHeight;
+            const nratio = nw / nh;
+
+            if (nw <= w && nh <= h) {
+                // Smaller than targeted width and height, do nothing
+                w = nw;
+                h = nh;
+            }
+            else {
+                if (nratio >= ratio) {
+                    // Retain width, calculate height
+                    h = w / nratio;
+                }
+                else {
+                    // Retain height, calculate width
+                    w = h * nratio;
+                }
+            }
+            // --------------------------------------------
+
+            const can = document.createElement('canvas');
+            can.width  = w;
+            can.height = h;
+            can.getContext('2d').drawImage(img, 0, 0, w, h);
+
+            // Resolve to dataURL
+            resolve(can.toDataURL());
+        };
+
+        img.onerror = e => {
+            URL.revokeObjectURL(img.src);
+            reject('ERROR: File is not an image');
+        };
+
+        img.src = URL.createObjectURL(f);
+    });
+}
